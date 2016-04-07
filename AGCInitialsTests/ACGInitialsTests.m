0aa5644c724f7ca6184dcc6293d32f3f36f0f391
Raiden996 <andreacipriani89@gmail.com>
2016-04-07 14:48:36 +0200

    Image must exist test

--- AGCInitialsTests/ACGInitialsTests.m
+++ AGCInitialsTests/ACGInitialsTests.m
@@ -4,19 +4,30 @@
 //
 
 #import <XCTest/XCTest.h>
-
+#import "UIImageView+AGCInitials.h"
 @interface AGCInitialsTests : XCTestCase
+
+@property UIImageView* imageView;
 
 @end
 
 @implementation AGCInitialsTests
 
-- (void)setUp {
+- (void)setUp
+{
+    _imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
     [super setUp];
 }
 
-- (void)tearDown {
+- (void)tearDown
+{
     [super tearDown];
+}
+
+-(void)testImageWithACGInitialsExist
+{
+    [_imageView agc_setImageWithInitials:@"AC"];
+    NSAssert(_imageView.image, @"Image is nil after setting with initials");
 }
 
 @end
