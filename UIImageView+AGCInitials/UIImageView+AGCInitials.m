02394afb844dcf58cadb95c4439350a79eb81600
Raiden996 <andreacipriani89@gmail.com>
2016-04-11 09:09:56 +0200

    UIImageView+AGCInitials - Added implementation with attributes

--- UIImageView+AGCInitials/UIImageView+AGCInitials.m
+++ UIImageView+AGCInitials/UIImageView+AGCInitials.m
@@ -3,47 +3,86 @@
 //
 //
 
-#import "UIImageView+AGCInitials.h"
 #import "AGCInitialsColors.h"
+#import "UIImageView+AGCInitials.h"
 
 @implementation UIImageView (AGCInitials)
 
 - (void)agc_setImageWithInitials:(nonnull NSString*)initials
 {
-    UIColor* colorForInitials = [[AGCInitialsColors sharedInstance] colorForString:initials];
-    [self acg_fillImageWithColor:colorForInitials];
+    NSDictionary* defaultTextAttributes = @{ NSFontAttributeName : [UIFont systemFontOfSize:40],
+                                             NSForegroundColorAttributeName : [UIColor whiteColor]
+                                             };
+    [self agc_setImageWithInitials:initials andTextAttributes:defaultTextAttributes];
+}
+
+- (void)agc_setImageWithInitials:(nonnull NSString*)initials andTextAttributes:(nonnull NSDictionary*)textAttributes
+{
+    NSString* uppercaseInitials = [initials uppercaseString];
+    UIColor* colorForInitials = [[AGCInitialsColors sharedInstance] colorForString:uppercaseInitials];
+    self.backgroundColor = colorForInitials;
+    self.image = [self acg_imageWithInitials:uppercaseInitials withTextAttributes:textAttributes];
 }
 
 - (void)agc_setImageWithInitialsFromName:(nonnull NSString*)name separatedByString:(nonnull NSString*)separator
 {
-    
+    NSString* initials = [self initialsFromName:name separatedByString:separator];
+    NSString* uppercaseInitials = [initials uppercaseString];
+    [self agc_setImageWithInitials:uppercaseInitials];
+}
+
+- (void)agc_setImageWithInitialsFromName:(nonnull NSString*)name separatedByString:(nonnull NSString*)separator withTextAttributes:(nonnull NSDictionary*)textAttributes
+{
+    NSString* initials = [self initialsFromName:name separatedByString:separator];
+    NSString* uppercaseInitials = [initials uppercaseString];
+    [self agc_setImageWithInitials:uppercaseInitials];
+}
+
+- (NSString*)initialsFromName:(NSString*)name separatedByString:(nonnull NSString*)separator
+{
+    NSArray* nameComponents = [name componentsSeparatedByString:separator];
+    if (nameComponents == nil || [nameComponents count] <= 0) {
+        return @"";
+    }
+    NSString* nameFirstComponent = nameComponents[0];
+    NSString* nameFirstComponentInitial = [nameFirstComponent substringToIndex:1];
+    NSString* nameSecondComponent = @"";
+    NSString* nameSecondComponentInitial = @"";
+    if ([nameComponents count] > 1) {
+        nameSecondComponent = nameComponents[1];
+        nameSecondComponentInitial = [nameSecondComponent substringToIndex:1];
+    }
+    return [nameFirstComponentInitial stringByAppendingString:nameSecondComponentInitial];
+}
+
+- (UIImage*)acg_imageWithInitials:(NSString*)initials withTextAttributes:(NSDictionary*)textAttributes
+{
+    [self acg_beginImageContext];
+    CGSize textSize = [initials sizeWithAttributes:textAttributes];
+    [initials drawInRect:[self acg_initialsRectForTextSize:textSize] withAttributes:textAttributes];
+    UIImage * newImage = UIGraphicsGetImageFromCurrentImageContext();
+    [self acg_endContext];
+    return newImage;
 }
 
 #pragma mark - Private utils
 
-- (void)acg_fillImageWithColor:(UIColor*)color
+- (CGContextRef)acg_beginImageContext
 {
-    CGContextRef context = [self acg_imageContext];
-    CGContextSetFillColorWithColor(context, color.CGColor);
-    CGContextFillRect(context, CGRectMake(0, 0, [self acg_imageWidth], [self acg_imageHeight]));
-    self.image = UIGraphicsGetImageFromCurrentImageContext();
-    [self acg_endContext];
-}
-
-- (CGContextRef)acg_imageContext
-{
-    UIGraphicsBeginImageContextWithOptions([self acg_imageSize], NO, [self acg_imageScale]);
+    UIGraphicsBeginImageContextWithOptions([self acg_imageSize], NO, 0);
     return UIGraphicsGetCurrentContext();
-}
-
-- (CGFloat)acg_imageScale
-{
-    return [UIScreen mainScreen].scale;
 }
 
 - (CGSize)acg_imageSize
 {
     return self.bounds.size;
+}
+
+- (CGRect)acg_initialsRectForTextSize:(CGSize)textSize
+{
+    CGFloat x = [self acg_imageWidth] / 2 - textSize.width / 2;
+    CGFloat y = [self acg_imageHeight] / 2 - textSize.height / 2;
+    return CGRectMake(x, y, textSize.width, textSize.height);
 }
 
 - (CGFloat)acg_imageWidth
