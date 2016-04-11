72edd79be21609cfe3c097642ed85bb03374349e
Raiden996 <andreacipriani89@gmail.com>
2016-04-11 22:06:44 +0200

    Updated example to show more imageviews with initials

--- AGCInitialsExample/AGCInitialsExampleViewController.m
+++ AGCInitialsExample/AGCInitialsExampleViewController.m
@@ -8,7 +8,12 @@
 
 @interface AGCInitialsExampleViewController ()
 
-@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView120;
+@property (weak, nonatomic) IBOutlet UIImageView *circularImageView120;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView60;
+@property (weak, nonatomic) IBOutlet UIImageView *circularImageView60;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView30;
+@property (weak, nonatomic) IBOutlet UIImageView *circularImageView30;
 
 @end
 
@@ -17,7 +22,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [_imageView agc_setImageWithInitials:@"AC"];
+    [_imageView120 agc_setImageWithInitialsFromName:@"Andrea Cipriani" separatedByString:@" "];
+    [_circularImageView120 agc_setImageWithInitials:@"AC"];
+    [_imageView60 agc_setImageWithInitialsFromName:@"Mick Jagger" separatedByString:@" "];
+    [_circularImageView60 agc_setImageWithInitials:@"MJ"];
+    [_imageView30 agc_setImageWithInitialsFromName:@"Keith Richards" separatedByString:@" "];
+    [_circularImageView30 agc_setImageWithInitials:@"KR"];
 }
 
 @end
